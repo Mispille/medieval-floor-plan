d312d49d3b768887c19ef0dcb78b3da678b6cac0
爪ΛᄃƧΉΛΛП <87117128+Mispille@users.noreply.github.com>
2026-04-08 17:51:39 +0200

    Add request to app.py to control parameters frrom URL and control seed

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import io
 
 import cairosvg
 import tomllib
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, request, send_file
 
 from generator import GeneratorConfig, generate
 from renderer import render_svg
@@ -23,12 +23,16 @@ def index():
     gen_config = app_config["generator"]
     rend_config = app_config["renderer"]
 
+    seed_param = request.args.get("seed")
+    seed = int(seed_param) if seed_param is not None else None
+
     config = GeneratorConfig(
-        width=gen_config["width"],
-        height=gen_config["height"],
-        min_room_size=gen_config["min_room_size"],
-        max_depth=gen_config["max_depth"],
-        room_margin=gen_config["room_margin"],
+        width=int(request.args.get("width", gen_config["width"])),
+        height=int(request.args.get("height", gen_config["height"])),
+        min_room_size=int(request.args.get("min_room_size", gen_config["min_room_size"])),
+        max_depth=int(request.args.get("max_depth", gen_config["max_depth"])),
+        room_margin=int(request.args.get("room_margin", gen_config["room_margin"])),
+        seed=seed
     )
 
     plan = generate(config)
@@ -36,7 +40,7 @@ def index():
     svg: str = render_svg(plan=plan, title="Maison medievale", cfg=rend_config)
     current_svg = svg
 
-    return render_template("index.html", svg=svg)
+    return render_template("index.html", svg=svg, seed=plan.seed)
 
 
 @app.route("/export")
