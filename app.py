7cb5586fe1ad6fce296f21bcc6c5be4fac8f1b23
Mispille <git.4wlxg@8alias.com>
2026-04-10 11:47:05 +0200

    Removed CairoSVG use and dependancy for better easy install on Windows

--- app.py
+++ app.py
@@ -1,21 +1,14 @@
-import io
-
-import cairosvg
 import tomllib
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request
 
 from generator import GeneratorConfig, generate
 from renderer import render_svg
 
 app = Flask(__name__)
 
-current_svg: str = ""
-
 
 @app.route("/")
 def index():
-    global current_svg
-
     # Import de la configuration
     with open("config.toml", "rb") as f:
         app_config = tomllib.load(f)
@@ -40,7 +33,6 @@ def index():
     plan = generate(config)
 
     svg: str = render_svg(plan=plan, title="Maison medievale", cfg=rend_config)
-    current_svg = svg
 
     return render_template(
         "index.html",
@@ -51,17 +43,4 @@ def index():
         min_room_size=config.min_room_size,
         max_depth=config.max_depth,
         room_margin=config.room_margin,
-    )
-
-
-@app.route("/export")
-def export_png():
-    buffer = io.BytesIO()
-    cairosvg.svg2png(bytestring=current_svg.encode("utf-8"), write_to=buffer)
-    buffer.seek(0)
-    return send_file(
-        path_or_file=buffer,
-        as_attachment=True,
-        mimetype="image/png",
-        download_name="maison.png",
     )
