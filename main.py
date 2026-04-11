6a52b50878c94d927e11cfb53cfd8e1e5d4ddfe4
爪ΛᄃƧΉΛΛП <87117128+Mispille@users.noreply.github.com>
2026-04-11 12:16:51 +0200

    Added argparse with --remote flag to open server to public network

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import argparse
+
 from app import app
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", debug=False)
+    parser = argparse.ArgumentParser(
+        description="Random medieval house generator for role-play games"
+    )
+    parser.add_argument("--remote", action="store_true", help="Open to public network")
+
+    args = parser.parse_args()
+    host = "0.0.0.0" if args.remote else "127.0.0.1"
+
+    app.run(host=host, debug=False)
